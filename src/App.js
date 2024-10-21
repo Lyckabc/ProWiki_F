dfba8fd97396a68aaff2e6f3646d00418b9b6921
lyckabc <dh2heart@gmail.com>
2024-10-21 23:16:27 +0900

    feat:user_LoginPage added

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import MainContent from './components/MainContent';
 import FolderSelector from './components/FolderSelector';
 import FolderStructure from './components/FolderStructure';
 import './styles/App.css';
+import User from './pages/Login';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <QueryClientProvider client={queryClient}>
       <div className="app">
         <Header />
@@ -20,6 +22,20 @@ function App() {
           <FolderStructure />
         </div>
         <FolderSelector />
+=======
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/about" element={<About />} /> */}
+          <Route path="/bookcase" element={<Bookcase />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/quest" element={<Quest />} />
+          <Route path="/arena" element={<Arena />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/login" element={<User />} />
+        </Routes>
+>>>>>>> Stashed changes
       </div>
     </QueryClientProvider>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,22 @@
+<<<<<<< Updated upstream
 import React from 'react';
+=======
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Header.css';
+>>>>>>> Stashed changes
 
 function Header() {
+  const [isLogin, setIsLogin] = useState(false);
+
+  const handleLogin = () => {
+    setIsLogin(true);
+  };
+
+  const handleLogout = () => {
+    setIsLogin(false);
+  };
+
   return (
     <header>
       <nav>
@@ -13,8 +29,22 @@ function Header() {
           <li>Map</li>
         </ul>
       </nav>
+<<<<<<< Updated upstream
       <div className="search">
         <input type="text" placeholder="Search here" />
+=======
+      <div className="user">
+        {isLogin ? (
+          <div className="login-user">
+            <span>Lyckabc</span>
+            <button><Link to="/user" className='user-menu' onClick={handleLogout}>My Menu</Link></button>
+          </div>
+        ) : (
+          <div className="customer-user">
+            <button><Link to="/login" className='user-menu' onClick={handleLogin}>Login</Link></button>
+          </div>
+        )}
+>>>>>>> Stashed changes
       </div>
       <div className="user">Lyckabc</div>
     </header>
